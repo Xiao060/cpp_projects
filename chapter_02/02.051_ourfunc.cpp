6516aa58bf1ee744b97d94d1897eac487d2ca5ed
Xiao <liangshengxiao1@163.com>
2023-03-22 20:08:10 +0800

    1

--- chapter_02/02.051_ourfunc.cpp
+++ chapter_02/02.051_ourfunc.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 void simon(int);                // 函数原型,指明参数、返回值类型,放于main函数前
 
+using namespace std;            // 共用一个名称空间,放于2函数前
+
 int main(){
-    using namespace std;        // 各自使用一个名称空间,见1
     simon(3);
     cout << "Pick an integer：";
     int count;
@@ -13,7 +14,6 @@ int main(){
 }
 
 void simon(int n){
-    using namespace std;        // 各自使用一个名称空间,见1
     cout << "Simon says touch your toes " << n << " times." << endl; 
 
 }
