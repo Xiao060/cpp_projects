c2005f88577ae0bf10eb49858c58b3cbe7014e89
Xiao <liangshengxiao1@163.com>
2023-03-23 10:34:55 +0800

    1

---
+++ chapter_03/03.050_chartype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+
+int main(){
+    using namespace std;
+    char ch;
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;                          // 字符类型接受一项
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+    return 0;
+}
