99d2475b0bbbc97122636fc0e923eabdf1bb6752
Xiao <liangshengxiao1@163.com>
2023-03-28 09:42:37 +0800

    1

---
+++ wd_practices/wd_basic01.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main(){
+    using namespace std;
+
+    double x;
+    cout << "请输入x的值: ";
+    cin >> x;
+
+    if (x < 5)
+        cout << x << endl;
+    else if (5 <= x < 15)
+        cout << x+6 << endl;
+    else
+        cout << x-6; 
+
+    return 0;
+}
---
+++ wd_practices/wd_basic02.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main(){
+    using namespace std;
+
+    char lower_letter;
+    cout << "请输入小写字母: ";
+    cin >> lower_letter;
+
+    cout << lower_letter;
+
+    return 0;
+}
