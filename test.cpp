6e0e846f48e3a52453efb3118b79c6db381f9d6e
Xiao <liangshengxiao1@163.com>
2023-03-29 22:31:52 +0800

    1

--- test.cpp
+++ test.cpp
@@ -20,7 +20,7 @@ union testType2{
 int main(){
     using namespace std;
 
-
+    /*
     char m[10] = "1248";            //{'1', '2', '5', '\0'};
     cout << m << endl;
 
@@ -33,7 +33,7 @@ int main(){
     // const string y = "1233";                // 错误方法
     string y = "1233";
     cout << y << endl;
-
+    */
 
     return 0;
 }
