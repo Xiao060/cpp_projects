2fe9f0b2be7c50f40c9fbe8b0deda8430c1c194f
Xiao <liangshengxiao1@163.com>
2023-06-08 15:06:16 +0800

    1

---
+++ chapter_10/10.07-09/10.090_usestock2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include "10.070_stock20.h"
+
+// 因为头文件中引入了 std, 此处不必在引入, 否则会产生混淆
+// using namespace std;
+const int STKS = 4;
+
+int main(){
+    Stock stocks[STKS] = {
+        Stock("NanoSmart", 12, 20.0),
+        Stock("Boffo Objects", 200, 2.0),
+        Stock("Monolithic Obelisks", 130, 3.25),
+        Stock("Fleep Enterprises", 60, 6.5)
+    };
+
+    cout << "Stock holdings:\n";
+    for (int st=0; st<STKS; ++st)
+        stocks[st].show();
+    
+    const Stock * top = &stocks[0];
+
+    for (int st=1; st<STKS; ++st)
+        top = & top->topval(stocks[st]);
+    
+    cout << "\nMost valuable holding:\n";
+    top->show();
+
+    return 0;
+}
