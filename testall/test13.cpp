fb3f71b78e54123313e2296351ad9ee4e1948597
Xiao <liangshengxiao1@163.com>
2023-06-21 09:59:07 +0800

    1

--- testall/test13.cpp
+++ testall/test13.cpp
@@ -2,10 +2,53 @@
 
 using namespace std;
 
+struct LNode{
+    int data;
+    struct LNode * next;
+};
+
+void show(LNode * h);
+void cz(LNode * h);
+
+
 int main(){
-    int b;
-    cin >> b;
-    cout << b%10;
+    LNode * a = new LNode;
+    LNode * b = new LNode;
+    LNode * c = new LNode;
+
+    b->data = 12;
+    a->next = b;
+    c->data = 13;
+    b->next = c;
+    c->next = NULL;
+
+    show(a);
 
     return 0;
+}
+
+void cz(LNode * h){
+    LNode *p, *q;
+    p = h;
+    int x;
+
+    cout << "请输入: ";
+    while (cin >> x){
+        q = new LNode;
+        q->data = x;
+        q->next = nullptr;
+        p->next = q;
+        p = q;
+    }
+}
+
+void show(LNode * h){
+    LNode * p = h->next;
+
+    while (p){
+        cout << p->data << " ";
+        p = p->next;
+    }
+ 
+    cout << endl;
 }
--- wd_practices/1.C/wd_basic27______.cpp
+++ wd_practices/1.C/wd_basic27______.cpp
@@ -2,8 +2,73 @@
 
 using namespace std;
 
-int main(){
+struct LNode{
+    int data;
+    struct LNode * next;
+};
 
+void create(LNode * h);
+void del(LNode * h, int x);
+void show(LNode * h);
+
+int main(){
+    LNode * h = new LNode;
+    
+    create(h);
+    show(h);
+
+    del(h, 12);
+    show(h);
+
+    del(h, 3);
+    show(h);
 
     return 0;
+}
+
+void create(LNode * h){
+    int x, n;
+    LNode * p = h;
+    LNode * q;
+
+    n = 1;
+    cout << "请输入第 " << n++ << " 个整数: ";
+    while (cin >> x){
+        q = new LNode;
+        q->data = x;
+        q->next = NULL;
+        p->next = q;
+        p = q;
+        cout << "请输入第 " << n++ << " 个整数: ";
+    }
+}
+
+void del(LNode * h, int x){
+    LNode * p = h;
+    LNode * q = p->next;
+
+    while (q){
+        if (q->data != x){
+            p = q;
+            q = q->next;
+        }else{
+            p->next = q->next;
+            delete q;
+            cout << x << " 节点已删除\n";
+            return ;
+        }
+    }
+
+    cout << x << " 节点不存在, 删除失败\n";
+}
+
+void show(LNode * h){
+    LNode * p = h->next;
+
+    while (p){
+        cout << p->data << " ";
+        p = p->next;
+    }
+ 
+    cout << endl;
 }
